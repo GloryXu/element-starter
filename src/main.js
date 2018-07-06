3c5f448229d29d38fef357275b64a1b971d753eb
xuguangrong <guangrong_xu@126.com>
2018-07-06 10:15:00 +0800

    上传本地

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
-import ElementUI from 'element-ui'
+// import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import { Button, Select, Notification, Input } from 'element-ui';
 import App from './App.vue'
 
-Vue.use(ElementUI)
+// Vue.use(ElementUI)
+Vue.use(Select)
+Vue.use(Button)
+Vue.use(Input)
+Vue.prototype.$notify = Notification;
 
 new Vue({
   el: '#app',
